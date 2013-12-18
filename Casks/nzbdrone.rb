6d3ba84c3644e0a6da3ce0bb2ab56adbc9eb538c
kay.one <kay.one@gmail.com>
2013-12-17 18:29:46 -0800

    updated nzbdrone cask

--- Casks/nzbdrone.rb
+++ Casks/nzbdrone.rb
@@ -1,5 +1,6 @@
-class MonoMre < Cask
+class NzbDrone < Cask
   url 'http://update.nzbdrone.com/v2/master/latest/NzbDrone.master.tar.gz'
   homepage 'http://www.nzbdrone.com'
   version '2.0.0'
+  no_checksum
 end
