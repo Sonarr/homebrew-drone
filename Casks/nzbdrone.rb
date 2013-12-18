bb100dfc84c697e520194a49e165b552a3f8c893
kay.one <kay.one@gmail.com>
2013-12-17 18:46:18 -0800

    added app link

--- Casks/nzbdrone.rb
+++ Casks/nzbdrone.rb
@@ -1,6 +1,7 @@
 class Nzbdrone < Cask
-  url 'http://update.nzbdrone.com/v2/master/latest/NzbDrone.master.zip'
+  url 'http://update.nzbdrone.com/v2/master/latest/NzbDrone.master.tar.gz'
   homepage 'http://www.nzbdrone.com'
   version '2.0.0'
   no_checksum
+  link 'NzbDrone.exe'
 end
