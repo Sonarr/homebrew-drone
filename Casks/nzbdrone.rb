d87ee6835f7320cec2a97121a1c46e1cc6b6a8cc
kay.one <kay.one@gmail.com>
2013-12-17 18:31:25 -0800

    updated cask to use zip

--- Casks/nzbdrone.rb
+++ Casks/nzbdrone.rb
@@ -1,5 +1,5 @@
 class NzbDrone < Cask
-  url 'http://update.nzbdrone.com/v2/master/latest/NzbDrone.master.tar.gz'
+  url 'http://update.nzbdrone.com/v2/master/latest/NzbDrone.master.zip'
   homepage 'http://www.nzbdrone.com'
   version '2.0.0'
   no_checksum
